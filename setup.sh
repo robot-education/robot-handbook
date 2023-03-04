7da3637eae3c8d30773cdb723018300585019a77
Alex Kempen <alex.bookreader@gmail.com>
2023-03-04 02:57:03 +0000

    Added various plate animations

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
+sudo apt-get install -y build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg texlive
 
 # install pip requirements
 pip install -r requirements.txt
