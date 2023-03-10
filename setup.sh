eac18387b80539be7865919fe6ccc17e960bdc10
Alex Kempen <alex.bookreader@gmail.com>
2023-03-10 00:02:27 +0000

    Change docker to use python image

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
+# manim system libraries
+sudo apt-get install -y build-essential libcairo2-dev ffmpeg libpango1.0-dev texlive-full 
 
 # install pip requirements
 pip install -r requirements.txt
