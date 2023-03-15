164fc7a76d33d52a54fb615b60235b672bdc4bd4
Alex Kempen <alex.bookreader@gmail.com>
2023-03-15 00:42:43 +0000

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # kudos to https://stackoverflow.com/a/50193944/10717280
 
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
 
-setup(name='rc_lib', version='0.1', packages=[
-    "rc_lib"
-])
+setup(name="rc_lib", version="0.1", packages=[ "rc_lib" ])
