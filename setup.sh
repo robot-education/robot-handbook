26251021d75d7ff97f284ad52bc95f25ff4466b8
Alex Kempen <alex.bookreader@gmail.com>
2023-03-19 03:40:40 +0000

    Changed python and pip to use 3.11?

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ apt-get install -y build-essential libcairo2-dev libpango1.0-dev ffmpeg texlive 
 # source .venv/bin/activate
 
 # install pip requirements
-pip3.11 install -r requirements.txt
+pip install -r requirements.txt
 
 # add the robot concepts helper library so we can import it anywhere
 # this will install any packages specified in setup.py
-pip3.11 install -e .
+pip install -e .
