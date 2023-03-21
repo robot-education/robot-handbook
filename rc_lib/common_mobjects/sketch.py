2586f61a3a304161472e09c626a9896e68133ec2
Alex Kempen <alex.bookreader@gmail.com>
2023-03-21 17:34:02 +0000

    Added other Sketch entity types to sketch.py

--- rc_lib/common_mobjects/sketch.py
+++ rc_lib/common_mobjects/sketch.py
@@ -1,10 +1,53 @@
+"""
+A module defining sketcher-like entities.
+The following convention is adopted - a vertex is a physical entity (usually a dot), 
+whereas a point is a piece of data.
+"""
 import manim as mn
 
 from typing import cast, Sequence, Self
+from abc import ABC, abstractmethod
 import enum
 
 from rc_lib.math_utils import vector
 from rc_lib.style import color
+
+
+class Sketch(ABC):
+    """
+    An abstract base class for Sketch entities.
+    """
+
+    @abstractmethod
+    def draw(self) -> mn.Animation:
+        pass
+
+
+class SketchCircle(Sketch, mn.VGroup):
+    def __init__(self, circle: mn.Circle, vertex: mn.Dot) -> None:
+        self.circle = circle
+        self.vertex = vertex
+        super().__init__(self.circle, self.point)
+
+    def center_point(self) -> vector.Vector2d:
+        return self.get_center()
+
+    def draw(self) -> mn.Animation:
+        return mn.Succession(
+            mn.Create(self.vertex, run_time=0), mn.GrowFromCenter(self.circle)
+        )
+
+
+class SketchPoint(Sketch, mn.VGroup):
+    def __init__(self, vertex: mn.Dot) -> None:
+        self.vertex = vertex
+        super().__init__(self.point)
+
+    def point(self) -> vector.Vector2d:
+        return self.get_center()
+
+    def draw(self) -> mn.Animation:
+        return mn.Create(self)
 
 
 class LineEnd(enum.IntEnum):
@@ -12,18 +55,13 @@ class LineEnd(enum.IntEnum):
     END = 1
 
 
-class SketchLine(mn.VGroup):
-    def __init__(
-        self,
-        start_point: vector.Point2d,
-        end_point: vector.Point2d,
-        color: color.Color = color.FOREGROUND,
-    ) -> None:
+class SketchLine(Sketch, mn.VGroup):
+    def __init__(self, line: mn.Line, start_vertex: mn.Dot, end_vertex: mn.Dot) -> None:
         self._color = color
-        self.line = mn.Line(start_point, end_point, color=color)
-        self.start = mn.Dot(start_point, color=color)
-        self.end = mn.Dot(end_point, color=color)
-        super().__init__(self.line, self.start, self.end)
+        self.line = line
+        self.start_vertex = start_vertex
+        self.end_vertex = end_vertex
+        super().__init__(self.line, self.start_vertex, self.end_vertex)
 
     def set_position(self, new_point: vector.Point2d, line_end: LineEnd) -> Self:
         new_coords = cast(Sequence[float], new_point)
@@ -35,21 +73,24 @@ class SketchLine(mn.VGroup):
             self.line.put_start_and_end_on(
                 cast(Sequence[float], self.start_point()), new_coords
             )
-        self.tip(line_end).move_to(new_point)
+        self.vertex(line_end).move_to(new_point)
         return self
 
-    def tip(self, line_end: LineEnd):
+    def vertex(self, line_end: LineEnd) -> mn.Dot:
         return getattr(self, ("start", "end")[line_end])
 
+    def point(self, line_end: LineEnd) -> vector.Point2d:
+        return self.vertex(line_end).get_center()
+
     def start_point(self) -> vector.Point2d:
-        return self.start.get_center()
+        return self.point(LineEnd.START)
 
     def end_point(self) -> vector.Point2d:
-        return self.end.get_center()
+        return self.point(LineEnd.END)
 
     def draw(self) -> mn.Animation:
         return mn.Succession(
-            mn.Create(self.start, run_time=0),
+            mn.Create(self.start_vertex, run_time=0),
             mn.Create(self.line),
-            mn.Create(self.end, run_time=0),
+            mn.Create(self.end_vertex, run_time=0),
         )
