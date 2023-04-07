f1bc227e7ab94c8138d2b9239a4e2b8ac4c71fcb
Alex Kempen <alex.bookreader@gmail.com>
2023-04-07 17:59:52 +0000

    Fixed dockerfile

--- animations/moving.py
+++ animations/moving.py
@@ -1,11 +1,11 @@
-import manim as mn
+from manim import *
 
 
-class MovingAround(mn.Scene):
+class MovingAround(Scene):
     def construct(self):
-        square = mn.Square(color=mn.BLUE, fill_opacity=1)
+        square = Square(color=BLUE, fill_opacity=1)
 
-        self.play(square.animate.shift(mn.LEFT))
-        self.play(square.animate.set_fill(mn.ORANGE))
+        self.play(square.animate.shift(LEFT))
+        self.play(square.animate.set_fill(ORANGE))
         self.play(square.animate.scale(0.3))
         self.play(square.animate.rotate(0.4))
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y build-essential libcairo2-dev libpango1.0-dev ffmpeg # texlive texlive-latex-extra
 
 # install command line utilities
-sudo apt-get install -y sudo man vim less
+sudo apt-get install -y man vim less
 
 # install pip requirements
 pip install -r requirements.txt
